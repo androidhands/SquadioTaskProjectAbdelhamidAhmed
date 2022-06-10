613264c3aff01521f7117c0ddd7f0ed98adfebfa
abdelhamid ahmed <abdelhamid.ahmed.abdo@gmail.com>
2022-06-10 21:29:27 +0200

    update dependency

--- lib/app_injection_container.dart
+++ lib/app_injection_container.dart
@@ -1,7 +1,6 @@
-
-import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:squadio_task_project_abdelhamid_hamed/core/local_database/app_database.dart';
 import 'package:squadio_task_project_abdelhamid_hamed/core/network_info/network_info.dart';
 import 'package:squadio_task_project_abdelhamid_hamed/core/utils/app_constants.dart';
@@ -16,17 +15,14 @@ import 'package:squadio_task_project_abdelhamid_hamed/features/popular_people/da
 import 'package:squadio_task_project_abdelhamid_hamed/features/popular_people/domain/repositories/popular_people_repository.dart';
 import 'package:squadio_task_project_abdelhamid_hamed/features/popular_people/domain/usecases/get_all_popular_people_use_case.dart';
 import 'package:squadio_task_project_abdelhamid_hamed/features/popular_people/presentation/bloc/popular_people_bloc.dart';
+
 final sl = GetIt.instance;
 
-
-void init(){
-  
-  
-
-
+void init() {
   //bloc
   sl.registerFactory(() => PopularPeopleBloc(getPopularPeopleUseCase: sl()));
-  sl.registerFactory(() => ActorDetailsBloc(getActorDetailUseCase: sl(), getActorImagesUseCase: sl()));
+  sl.registerFactory(() => ActorDetailsBloc(
+      getActorDetailUseCase: sl(), getActorImagesUseCase: sl()));
 
   //usecases
   sl.registerLazySingleton(() => GetPopularPeopleUseCase(sl()));
@@ -34,20 +30,27 @@ void init(){
   sl.registerLazySingleton(() => GetActorImagesUseCase(sl()));
   //repositories
 
-  sl.registerLazySingleton<PopularPeopleRepository>(() => PopularPeopleRepositoryImpl(popularPeopleRemoteDataSource: sl()
-      ,peopleLocalDataSource:sl(), platformNetworkInfo: sl()));
-  sl.registerLazySingleton<ActorDetailsRepository>(() => ActorDetailsRepositoryImpl(actorDetailsRemoteDataSource: sl(), platformNetworkInfo: sl()));
+  sl.registerLazySingleton<PopularPeopleRepository>(() =>
+      PopularPeopleRepositoryImpl(
+          popularPeopleRemoteDataSource: sl(),
+          peopleLocalDataSource: sl(),
+          platformNetworkInfo: sl()));
+  sl.registerLazySingleton<ActorDetailsRepository>(() =>
+      ActorDetailsRepositoryImpl(
+          actorDetailsRemoteDataSource: sl(), platformNetworkInfo: sl()));
 
   //data sources
 
-  sl.registerLazySingleton<PopularPeopleRemoteDataSource>(() => PopularPeopleRemoteDataSourceImpl(sl(), AppConstants.baseUrl));
-  sl.registerLazySingleton<ActorDetailsRemoteDataSource>(() => ActorDetailsRemoteDataSourceImpl(sl(),AppConstants.baseUrl));
-  sl.registerLazySingleton<PopularPeopleLocalDataSource>(() => PopularPeopleLocalDataSourceImpl(sl()));
+  sl.registerLazySingleton<PopularPeopleRemoteDataSource>(
+      () => PopularPeopleRemoteDataSourceImpl(sl(), AppConstants.baseUrl));
+  sl.registerLazySingleton<ActorDetailsRemoteDataSource>(
+      () => ActorDetailsRemoteDataSourceImpl(sl(), AppConstants.baseUrl));
+  sl.registerLazySingleton<PopularPeopleLocalDataSource>(
+      () => PopularPeopleLocalDataSourceImpl(sl()));
 
   //database
-  sl.registerLazySingleton(() => DataConnectionChecker());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
   sl.registerLazySingleton(() => http.Client());
   sl.registerLazySingleton(() => AppDatabase());
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
-  
 }
